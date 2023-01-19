fea676fff9eaf0d6d566d022868a5789fdb555d6
Humam <hu666mam@gmail.com>
2023-01-19 18:19:51 +0100

    fix check win and draw functions

--- app.js
+++ app.js
@@ -10,6 +10,16 @@ const game = (function() {
     const X_TURN = 'X';
     const O_TURN = 'O';
     let playerTurn = X_TURN;
+
+    const A = GAMEBOARD[0];
+    const B = GAMEBOARD[1];
+    const C = GAMEBOARD[2];
+    const D = GAMEBOARD[3];
+    const E = GAMEBOARD[4];
+    const F = GAMEBOARD[5];
+    const G = GAMEBOARD[6];
+    const H = GAMEBOARD[7];
+    const I = GAMEBOARD[8];
     
     const render = () => {
         for ( let i = 0 ; i < GAMEBOARD.length ; i++) {
@@ -22,14 +32,42 @@ const game = (function() {
         else playerTurn = X_TURN;
     }
     const checkWin = () => {
-        
+        if ( A === B && B === C && A !== ''  ) {
+            log(`Player ${playerTurn} won!`);
+        }
+        if ( D === E && E === F && D !== '' ) {
+            log(`Player ${playerTurn} won!`);
+        }
+        if ( G === H && H === I && G !== '' ) {
+            log(`Player ${playerTurn} won!`);
+        }
+        if ( A === D && D === G && A !== '' ) {
+            log(`Player ${playerTurn} won!`);
+        }
+        if ( B === E && E === H && B !== '' ) {
+            log(`Player ${playerTurn} won!`);
+        }
+        if ( C === F && F === I && C !== '' ) {
+            log(`Player ${playerTurn} won!`);
+        }
+        if ( A === E && E === I && A !== '' ) {
+            log(`Player ${playerTurn} won!`);
+        }
+        if ( C === E && E === G && C !== '' ) {
+            log(`Player ${playerTurn} won!`);
+        }
+    }
+    const checkDraw = () => {
+        if ( A !== '' && B !== '' && C !== '' && D !== '' && E !== '' && F !== '' && G !== '' && H !== '' && I !== '') {
+            log("It's a draw");
+        }
     }
     const tickBox = (e) => {
         const targetBox = e.target.getAttribute('data-index');
         GAMEBOARD[targetBox] = playerTurn;
         render();
         checkWin();
-        // checkDraw();
+        checkDraw();
         switchPlayer();
     }
     const addEvent = () => {
